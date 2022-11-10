30268370c274eb779ed49e754c5f696a09be66e1
Sara Park <sspark106@gmail.com>
2022-11-10 12:49:11 -0700

    Add revisions

--- cleanUp.js
+++ cleanUp.js
@@ -7,40 +7,54 @@
     // using `node cleanUp.js` or copy and paste into a replit
 
 
-// EX 1:
-function nameQuestion() {
-  console.log("Hello, what is your name?")
+// EX 1: 
+// I changed the funtion name to "askName" for a more concise function name. I also included a
+// semicolon at the end of the statement to follow function syntax. 
+// I changed the function call to askName() and added a semicolon to the call for proper function syntax.
+function askName() {
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+askName();
 
 
 // EX 2:
+// I indented statements in the code block, added a semicolon to the first statement, 
+// and placed the end curly bracket to the left for better syntax. I also replaced console.log 
+// with return for more flexibility in using the function. 
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third;
+  return sum;
+}
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
 
 
 // EX 3:
-func makeFreshPesto(){
+// I replaced func with the complete word, function, added a space between () and {}, and
+// moved the end curly bracket to a single line at the bottom for better syntax and readability. 
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+// I changed the function name to "calculateAverage" for a more concise function name. 
+// I moved the first curly bracket to the top line, indented the first statement of the code block,
+// removed the line between statements, added a semicolon to the last statement, and moved 
+// the end bracket to the left for better readability.
+// I also called the function with arguments so the code runs.
+function calculateAverage(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  console.log(`the average is: ${avg}.`);
+}
 
-  console.log(`the average is: ${avg}`.)
-  }
+calculateAverage(2, 4);
